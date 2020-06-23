8fa13fbceedbca57566b7ba237c8347a67813e68
Ethan Kent <ethan.kent@amount.com>
2020-06-23 08:43:03 -0500

    Working sort of but with cross-compile issues

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,5 +1,10 @@
-# docker build . -t verus:latest
+docker build . -t verus:latest
 docker create -it --name dummy verus:latest bash
 docker cp dummy:/verus/verus-wasm/pkg www/
-docker cp dummy:/verus/verus-validations/target/release/libverus_validations.dylib verus-gem/bin/
+docker cp dummy:/verus/verus-ruby-wrappers/target/release/libverus_ruby_wrappers.dylib verus-gem/bin/
 docker rm -f dummy
+
+cd ruby-server
+bundle
+cd ..
+
--- verus-gem/lib/verus-gem.rb
+++ verus-gem/lib/verus-gem.rb
@@ -3,7 +3,7 @@ require "ffi"
 module Verus
   extend FFI::Library
 
-  ffi_lib "./bin/libverus_ruby_wrappers." + FFI::Platform::LIBSUFFIX
+  ffi_lib "../verus-gem/bin/libverus_ruby_wrappers.so"
 
   attach_function :validate_email, :email, [:string], :bool
   attach_function :validate_date, :date, [:string], :bool
