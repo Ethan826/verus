6273df7a407838781ae84b09981f7f049ef0290b
Ethan Kent <ekent@jd11.law.harvard.edu>
2020-06-22 20:56:43 -0500

    Implement validations, include in WASM

--- verus-validations/src/lib.rs
+++ verus-validations/src/lib.rs
@@ -1,6 +1,27 @@
+#[macro_use]
+extern crate lazy_static;
+
+use chrono::NaiveDate;
+use regex::Regex;
+
+lazy_static! {
+    static ref EMAIL_REGEX: Regex = Regex::new(
+        r#"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$"#
+    ).unwrap();
+
+    static ref DATE_FORMATS: Vec<&'static str> = vec!["%F", "%x", "%m/%d/%Y"];
+}
+
 #[no_mangle]
-pub extern "C" fn doubler(x: i32) -> i32 {
-    x * 2
+pub extern "C" fn validate_email(email: &str) -> bool {
+    EMAIL_REGEX.is_match(email)
+}
+
+#[no_mangle]
+pub extern "C" fn validate_date(date: &str) -> bool {
+    DATE_FORMATS
+        .iter()
+        .any(|format| NaiveDate::parse_from_str(date, format).is_ok())
 }
 
 #[cfg(test)]
@@ -8,7 +29,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_doubler() {
-        assert_eq!(doubler(2), 4);
+    fn test_validate_email_valid() {
+        assert!(validate_email("ethan@kent.com"));
+    }
+
+    #[test]
+    fn test_validate_email_invalid() {
+        assert!(!validate_email("ethan@kent@com"));
+    }
+
+    #[test]
+    fn test_validate_date_valid() {
+        assert!(validate_date("1981-08-26"));
+    }
+
+    #[test]
+    fn test_validate_date_invalid() {
+        assert!(!validate_date("1981-02-29"));
     }
 }
--- verus-wasm/src/lib.rs
+++ verus-wasm/src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use verus_validations::doubler;
+use verus_validations;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,12 +9,12 @@ use verus_validations::doubler;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[wasm_bindgen(js_name = validateEmail)]
+pub fn wrapper_validate_email(email: &str) -> bool {
+    verus_validations::validate_email(email)
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert(&doubler(3).to_string());
+#[wasm_bindgen(js_name = validateDate)]
+pub fn wrapper_validate_date(date: &str) -> bool {
+    verus_validations::validate_date(date)
 }
