37bcf6be12330ad5b648c70c015cf1578050244c
Ethan Kent <ethan.kent@amount.com>
2020-06-23 07:04:51 -0500

    Working WASM validations

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
 import { validateEmail, validateDate } from "verus-wasm";
 
-console.log(validateDate);
+const emailInput = document.getElementById("email-input");
+const emailWasm = document.getElementById("email-wasm");
+const emailRuby = document.getElementById("email-ruby");
+
+const dateInput = document.getElementById("date-input");
+const dateWasm = document.getElementById("date-wasm");
+const dateRuby = document.getElementById("date-ruby");
+
+const validityText = input => (input ? "Valid" : "Invalid");
+
+const emailWasmListener = () => emailWasm.innerText = validityText(validateEmail(emailInput.value));
+const dateWasmListener = () => dateWasm.innerText = validityText(validateDate(dateInput.value));
+
+emailInput.onkeyup = emailWasmListener;
+dateInput.onkeyup = dateWasmListener;
